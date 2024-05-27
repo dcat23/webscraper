a1cb39523021cf718d0fb62c1dc3cf8bf9531ce8
Devin Catuns <devincatuns1@gmail.com>
2024-05-27 19:01:00 -0400

    wip

--- src/test/java/life/macchiato/WebScraperTest.java
+++ src/test/java/life/macchiato/WebScraperTest.java
@@ -4,14 +4,15 @@ import life.macchiato.webscraper.Node;
 import life.macchiato.webscraper.NodeList;
 import life.macchiato.webscraper.URLBuilder;
 import life.macchiato.webscraper.WebScraper;
-import org.htmlunit.html.HtmlAnchor;
-import org.htmlunit.html.HtmlPage;
+import org.htmlunit.html.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -66,16 +67,25 @@ class WebScraperTest {
     }
 
     @Test
-    void shouldSelectAll() {
-        URL url = urlBuilder.setQuery("microservice").build();
-        String selector = ".entry-title a[rel=\"bookmark\"]";
+    void shouldFindImages() {
+        URL url = urlBuilder.setQuery("spring").build();
 
         HtmlPage page = WebScraper.getPage(url);
+        List<DomElement> articles = page.getByXPath("//article[contains(@id, 'post')]");
+        List<String> images = new ArrayList<>();
 
-        NodeList nodes = WebScraper.selectAll(selector, url);
-        for (Node node : nodes.nodeList())
+        for (DomElement el : articles)
         {
+            String image = el.getElementsByTagName("img").get(0)
+                    .getAttribute("data-src");
+            String href = el.getElementsByTagName("a").get(0)
+                    .getAttribute("href");
+            String title = el.querySelector(".entry-title a[rel=\"bookmark\"]")
+                    .getTextContent();
 
+            images.add(image);
         }
+        assertThat(images.size()).isEqualTo(articles.size());
+
     }
 }
